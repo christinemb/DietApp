3c83cc3b60e6b842f4a5a3dbbee8febfc7432fcb
unknown <wattersonglendon@yahoo.co.uk>
2015-04-29 11:05:41 +0100

    another change

--- www/js/app.js
+++ www/js/app.js
@@ -29,43 +29,27 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     templateUrl: "templates/menu.html",
     controller: 'AppCtrl'
   })
+  
+.state('app.dishes', {
+-    url: "/search",
++    url: "/dishes/:DishesId",
+     views: {
+     views: {
+       'menuContent': {
+       'menuContent': {
+-        templateUrl: "templates/search.html"
++        templateUrl: "templates/dishes.html",
+-      }
++        controller: 'DishesCtrl'
+-    }
 
-  .state('app.search', {
-    url: "/search",
+  
     views: {
       'menuContent': {
         templateUrl: "templates/search.html"
-      }
-    }
-  })
-
-  .state('app.browse', {
-    url: "/browse",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/browse.html"
-      }
-    }
-  })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: "/playlists/:playlistId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/playlist.html",
-        controller: 'PlaylistCtrl'
-      }
+    
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/dishes/1');
 });
